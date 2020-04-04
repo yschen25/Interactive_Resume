273b1cbd354aba4936fe9daf44186da057192be6
Ka <yschen25@gmail.com>
2020-04-04 23:34:38 +0800

    rose

---
+++ src/component/lock/lock.js
@@ -0,0 +1,150 @@
+import React from 'react';
+
+class Lock extends React.Component{
+    render(){
+        return(
+            <div>
+                <div className="loader"></div>
+                <div className="lock">
+                    <div className="screen">
+                        <div className="code"> 0000</div>
+                        <div className="status">LOCKED</div>
+                        <div className="scanlines"></div>
+                    </div>
+                    <div className="rows">
+                        <div className="row">
+                            <div className="cell">
+                                <div className="text">0</div>
+                            </div>
+                            <div className="cell">
+                                <div className="text">1</div>
+                            </div>
+                            <div className="cell">
+                                <div className="text">2</div>
+                            </div>
+                            <div className="cell">
+                                <div className="text">3</div>
+                            </div>
+                            <div className="cell">
+                                <div className="text">4</div>
+                            </div>
+                            <div className="cell">
+                                <div className="text">5</div>
+                            </div>
+                            <div className="cell">
+                                <div className="text">6</div>
+                            </div>
+                            <div className="cell">
+                                <div className="text">7</div>
+                            </div>
+                            <div className="cell">
+                                <div className="text">8</div>
+                            </div>
+                            <div className="cell">
+                                <div className="text">9</div>
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="cell">
+                                <div className="text">0</div>
+                            </div>
+                            <div className="cell">
+                                <div className="text">1</div>
+                            </div>
+                            <div className="cell">
+                                <div className="text">2</div>
+                            </div>
+                            <div className="cell">
+                                <div className="text">3</div>
+                            </div>
+                            <div className="cell">
+                                <div className="text">4</div>
+                            </div>
+                            <div className="cell">
+                                <div className="text">5</div>
+                            </div>
+                            <div className="cell">
+                                <div className="text">6</div>
+                            </div>
+                            <div className="cell">
+                                <div className="text">7</div>
+                            </div>
+                            <div className="cell">
+                                <div className="text">8</div>
+                            </div>
+                            <div className="cell">
+                                <div className="text">9</div>
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="cell">
+                                <div className="text">0</div>
+                            </div>
+                            <div className="cell">
+                                <div className="text">1</div>
+                            </div>
+                            <div className="cell">
+                                <div className="text">2</div>
+                            </div>
+                            <div className="cell">
+                                <div className="text">3</div>
+                            </div>
+                            <div className="cell">
+                                <div className="text">4</div>
+                            </div>
+                            <div className="cell">
+                                <div className="text">5</div>
+                            </div>
+                            <div className="cell">
+                                <div className="text">6</div>
+                            </div>
+                            <div className="cell">
+                                <div className="text">7</div>
+                            </div>
+                            <div className="cell">
+                                <div className="text">8</div>
+                            </div>
+                            <div className="cell">
+                                <div className="text">9</div>
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="cell">
+                                <div className="text">0</div>
+                            </div>
+                            <div className="cell">
+                                <div className="text">1</div>
+                            </div>
+                            <div className="cell">
+                                <div className="text">2</div>
+                            </div>
+                            <div className="cell">
+                                <div className="text">3</div>
+                            </div>
+                            <div className="cell">
+                                <div className="text">4</div>
+                            </div>
+                            <div className="cell">
+                                <div className="text">5</div>
+                            </div>
+                            <div className="cell">
+                                <div className="text">6</div>
+                            </div>
+                            <div className="cell">
+                                <div className="text">7</div>
+                            </div>
+                            <div className="cell">
+                                <div className="text">8</div>
+                            </div>
+                            <div className="cell">
+                                <div className="text">9</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Lock;
