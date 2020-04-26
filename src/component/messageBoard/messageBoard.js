7c2a4b003846a033cfd6e47303d27b8ba451fde4
Ka <yschen25@gmail.com>
2020-04-26 20:07:13 +0800

    pen

--- src/component/messageBoard/messageBoard.js
+++ src/component/messageBoard/messageBoard.js
@@ -6,45 +6,74 @@ class ConnectMessageBoard extends React.Component {
     constructor(props) {
         super(props);
         this.state = {count: 0};
+        this.focus = this.focus.bind(this);
+        this.blur = this.blur.bind(this);
         this.typeMessage = this.typeMessage.bind(this);
     }
 
+    focus() {
+        // Show the pen when focus on the textarea
+        document.getElementById('pen').style.display = 'block';
+    }
+
+    blur() {
+        // Hide the pen when focus on the textarea
+        document.getElementById('pen').style.display = 'none';
+    }
+
     typeMessage(e) {
-        let count = parseInt(e.target.getAttribute('data-count'));
-        let cursorPosition = document.getElementById('textarea').selectionStart;
-        let startPosition = 30 + cursorPosition;
+        let count = parseInt(e.target.getAttribute('data-count')); // Count type how many times
+        let penStyle = document.getElementById('pen').style;
+        let cursorPosition = document.getElementById('textarea').selectionStart; // Cursor position
+        let startPosition = 30 + cursorPosition; // Pen start position (left)
+        let topPosition = 278; // Pen start position (top)
 
         // Pen's initial position
-        document.getElementById('pen').style.left = startPosition + 'px';
+        penStyle.left = startPosition + 'px';
         count += 1;
         this.setState({count: count});
-
-        console.log('cursorPosition', cursorPosition);
 
         // Change pen's position
         if (count >= 2) {
             let textPosition = 8 * count;
             let nowPosition = startPosition + textPosition;
 
-            console.log('count', count);
-            console.log('nowPosition', nowPosition);
-            console.log('nowPosition % 2', nowPosition % 2);
-
             if (nowPosition % 2 === 0) {
-                document.getElementById("pen").style.transform = "rotate(-20deg)";
-                document.getElementById("pen").style.top = "270px";
-                document.getElementById('pen').style.left = nowPosition + 'px';
+                penStyle.transform = 'rotate(-20deg)';
+                penStyle.top = (topPosition - 5) + 'px';
+                penStyle.left = nowPosition + 'px';
             } else {
-                document.getElementById("pen").style.transform = "rotate(0deg)";
-                document.getElementById("pen").style.top = "275px";
-                document.getElementById('pen').style.left = nowPosition + 'px';
+                penStyle.transform = 'rotate(0deg)';
+                penStyle.top = topPosition + 'px';
+                penStyle.left = nowPosition + 'px';
+            }
+
+            // New line
+            let newLine = 74;
+            for (let i = 1; i <= 9; i++) {
+                if (cursorPosition >= newLine * i) {
+                    this.transferPosition(count, newLine * i, i);
+                }
             }
         }
+    }
 
-        // break line
-        if (cursorPosition === 74) {
-            document.getElementById('pen').style.left = startPosition + 'px';
-            document.getElementById('pen').style.top = 295 + 'px';
+    transferPosition(count, num, topNum) {
+        let penStyle = document.getElementById('pen').style;
+        let startPosition = 30;
+        let topPosition = 278;
+        let textPosition = 9 * (count - (num + 1));
+        let nowPosition = startPosition + textPosition;
+
+        penStyle.left = startPosition + 'px';
+        penStyle.top = (topPosition + 25 * topNum) + 'px';
+
+        if (nowPosition % 2 === 0) {
+            penStyle.transform = 'rotate(-20deg)';
+            penStyle.left = nowPosition + 'px';
+        } else {
+            penStyle.transform = 'rotate(0deg)';
+            penStyle.left = nowPosition + 'px';
         }
     }
 
@@ -55,10 +84,10 @@ class ConnectMessageBoard extends React.Component {
                 <p className="desc">Welcome all of you here to my interactive resume, wish you have fun here</p>
                 <div className="msg">
                     <label htmlFor="name">Name : </label>
-                    <input id="name" name="name" autoComplete="off" placeholder="Your name here"/>
+                    <input id="name" name="name" autoFocus autoComplete="off" placeholder="Your name here"/>
                     <img id="pen" className="pen" src={pen}/>
                     <textarea id="textarea" cols="40" rows="7" data-count={this.state.count}
-                              placeholder="Enter the message"
+                              onFocus={this.focus.bind(this)} onBlur={this.blur.bind(this)}
                               onKeyPress={this.typeMessage.bind(this)}></textarea>
                 </div>
             </div>
