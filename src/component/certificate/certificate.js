6b18f1ccd1fb4c7633cec17fa088bd6c908dd2f7
Ka <yschen25@gmail.com>
2020-06-06 19:05:58 +0800

    add mail

--- src/component/certificate/certificate.js
+++ src/component/certificate/certificate.js
@@ -19,6 +19,23 @@ class Certificate extends React.Component {
                     <p><b>Others</b> : Git, Webpack, NPM, Docker, PhpStorm, Sublime, Photoshop, Illustrator,
                         SonarQube, Docker, Web Design, UI/UX Design, Agile, SEO</p>
                 </div>
+                <div className="letter-image">
+                    <div className="animated-mail">
+                        <div className="back-fold"></div>
+                        <div className="letter">
+                            <div className="letter-border"></div>
+                            <div className="letter-title"></div>
+                            <div className="letter-context"></div>
+                            <div className="letter-stamp">
+                                <div className="letter-stamp-inner"></div>
+                            </div>
+                        </div>
+                        <div className="top-fold"></div>
+                        <div className="body"></div>
+                        <div className="left-fold"></div>
+                    </div>
+                    <div className="shadow"></div>
+                </div>
                 <div className="contactInfo item">
                     <a href="https://github.com/yschen25" target="_blank">Github</a>
                     <a href="https://www.linkedin.com/in/yschen25" target="_blank">LinkedIn</a>
